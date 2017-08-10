ceb39080d33e15ff948655a4b3f4f38b9b9b69cb
roy <rxlisbest@163.com>
2017-08-10 12:09:26 +0800

    修改接口位置

--- client/api.js
+++ client/api.js
@@ -1,7 +1,7 @@
 let localhostDev = false;
 //如果是本地开发，就把localhostDev 设置成true，连接就是拼成‘http://localhost:8080/api/test’
 //生产环境，就把localhostDev 设置成false，连接就是拼成‘http://xxx.com/api/test’
-let _host = localhostDev ? 'http://127.0.0.1:8000' : 'http://api.blog.ruixinglong.net';
+let _host = localhostDev ? 'http://127.0.0.1:8000' : 'https://api.blog.ruixinglong.net';
 let httpUrl = {
 	article: {
 		'index': _host + '/v1/article',
